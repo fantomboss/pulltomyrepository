386093b0f043a6864ce1dae64308223ab56b1c11
fantomboss <fantom_vip@126.com>
2019-01-22 22:04:31 +0800

    Initial commit

---
+++ src/main/java/com/fantom/pulltomyrepository/PulltomyrepositoryApplication.java
@@ -0,0 +1,14 @@
+package com.fantom.pulltomyrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulltomyrepositoryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PulltomyrepositoryApplication.class, args);
+  }
+
+}
+
